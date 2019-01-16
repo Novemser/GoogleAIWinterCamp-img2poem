5479a62bf32afd94f356d6f584fc1e271df7a202
Novemser <Novemser@gmail.com>
2019-01-16 17:34:14 +0800

    Add poem

--- img2poem/code/src/frontend/main.py
+++ img2poem/code/src/frontend/main.py
@@ -4,10 +4,49 @@ import os
 import sys
 # include parent dir
 sys.path.append('../')
+import nn_process
+print ('Loading Extracting Feature Module...')
+extract_feature = nn_process.create('extract_feature')
+print ('Loading Generating Poem Module...')
+generate_poem = nn_process.create('generate_poem')
+
+def get_poem(image_file):
+    """Generate a poem from the image whose filename is `image_file`
+
+    Parameters
+    ----------
+    image_file : str
+        Path to the input image
+
+    Returns
+    -------
+    str
+        Generated Poem
+    """
+    # Check whether the file exists
+    assert os.path.exists(image_file), FileNotFoundError(
+            'File `{}` not found.'.format(image_file))
+    assert not os.path.isdir(image_file), FileNotFoundError(
+            'The path should be a filename instead of `{}`.'.format(image_file))
+    img_feature = extract_feature(image_file)
+    return generate_poem(img_feature)
 
 app = Flask(__name__)
+full_filename = 'static/uploads/Screenshot_from_2018-12-28_19-41-28.png'
 
 s = "static/uploads"
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        s_path = os.path.join(s ,secure_filename(f.filename))
+        upload_path = os.path.join(basepath, s_path)
+        f.save(upload_path)
+
+        return render_template('index.html', user_image = upload_path, poem = get_poem(upload_path))
+    return render_template('index.html', user_image = '', poem = 'please upload')
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
@@ -21,7 +60,6 @@ def upload():
     
 @app.route('/index')
 def show_index():
-    full_filename = 'static/uploads/Screenshot_from_2018-12-28_19-41-28.png'
     return render_template("img.html", user_image = full_filename)
 
 if __name__ == '__main__':
